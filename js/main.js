06fc27a780a25de4d61d853c2de6fc8cd44a48c5
OdilbekDev <desmon.programmer@gmail.com>
2022-09-15 13:45:05 +0500

    1

---
+++ js/main.js
@@ -0,0 +1,74 @@
+let elList = document.querySelector('.list')
+let elSelect1 = document.querySelector('#select1')
+let elSelect2 = document.querySelector('#select2')
+let elSelect3 = document.querySelector('#select3')
+let newArr = []
+let newArr2 = []
+let newArr3 = []
+const allFilm = movies.slice(0,100)
+mapper(allFilm)
+
+function mapper(params) {
+    elList.innerHTML = ""
+    params.forEach((e, i)=>{
+        let newLi = document.createElement('li')
+        if(newArr.includes(e.Categories) != true){
+            newArr.push(e.Categories)
+        }
+        if(newArr2.includes(e.movie_year) != true){
+            newArr2.push(e.movie_year)
+        }
+        if(newArr3.includes(e.imdb_rating) != true){
+            newArr3.push(e.imdb_rating)
+        }
+        newLi.innerHTML = `<div class="card" style="width: 18rem;">
+        <img src="https://img.youtube.com/vi/<${e.ytid}>/default.jpg
+        " class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${e.Title}</h5>
+          <p>${e.Categories}</p>
+          <p>${e.movie_year}</p>
+          <p>${e.imdb_rating}</p>
+          <a href="https://www.youtube.com/watch?v=${e.ytid}" class="btn btn-primary">Watch Film</a>
+        </div>
+      </div>`
+      elList.appendChild(newLi)
+    })
+}
+
+
+newArr.map((e,i)=>{
+    let newOption = document.createElement('option')
+    newOption.text = e
+    elSelect1.appendChild(newOption)
+})
+
+elSelect1.addEventListener('change', (e) => {
+    let value = e.target.value
+    const categoryFilm = allFilm.filter((e)=> e.Categories == value)
+    mapper(categoryFilm)
+})
+
+newArr2.map((e,i)=>{
+    let newOption = document.createElement('option')
+    newOption.text = e
+    elSelect2.appendChild(newOption)
+})
+
+elSelect2.addEventListener('change', (e) => {
+    let value = e.target.value
+    const categoryFilm = allFilm.filter((e)=> e.movie_year == value)
+    mapper(categoryFilm)
+})
+
+newArr3.map((e,i)=>{
+    let newOption = document.createElement('option')
+    newOption.text = e
+    elSelect3.appendChild(newOption)
+})
+
+elSelect3.addEventListener('change', (e) => {
+    let value = e.target.value
+    const categoryFilm = allFilm.filter((e)=> e.imdb_rating == value)
+    mapper(categoryFilm)
+})
